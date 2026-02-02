92230cd2e7e465b30d40b7a55c0ddc02fb2d970a
seanlumasag <seanlumasag15@gmail.com>
2026-02-02 16:20:30 -0500

    Fix test files

--- backend/src/test/java/com/livemessaging/backend/controller/AuthIntegrationTest.java
+++ backend/src/test/java/com/livemessaging/backend/controller/AuthIntegrationTest.java
@@ -25,7 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthIntegrationTest {
 
     @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+    @SuppressWarnings("resource")
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test");
